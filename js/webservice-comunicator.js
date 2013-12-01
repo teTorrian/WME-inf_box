bb34485896a94f0542c413c322721a2e281048fa
Christoph Traeger <christoph.traeger@gmail.com>
2013-12-01 23:31:19 +0100

    - Sortierung integriert - Größendarstellung dynamisch gestaltet; je nach Größe Byte, KB, MB, GB - Größendarstellung auf eine Nachkommestelle beschränkt

--- js/webservice-comunicator.js
+++ js/webservice-comunicator.js
@@ -50,7 +50,6 @@ function getUser(userid) {
 
 function getUserItems(userid) {
 	var itemSource = source + "users/" + userid + "/items";
-	var i = 0;
 	
 	jQuery.getJSON(itemSource, function(data) {
 		$.each( data, function( key, val ) {
@@ -64,17 +63,10 @@ function getUserItems(userid) {
 			
 			getItemMeta(item);
 		});
-		
 	});
-
 }
 
-
-
-
 function getItemMeta(item) {
-
-	
 	var metadataSource = source + "items/" + item.id + "/metadata";
 	
 	jQuery.getJSON(metadataSource, function(data) {
@@ -86,23 +78,35 @@ function getItemMeta(item) {
 			$("#img-of-" + itemid).attr('style', 'display:none');
 		}
 		
-		$("#size-of-" + item.id).append((data.size / 1024).toFixed(2) + " MB");
+		$("#size-of-" + item.id).append(convertDataSize(data.size));
 		$("#type-of-" + item.id).append(data.mimetype);
 		$("#date-of-" + item.id).append(data.creation_date);
 		
 	});
 }
 
-function printItemIdArray() {
+function convertDataSize(byteSize) {
+	var size = byteSize;
+	var unit = 'Byte';
 	
-	$("body").append("printItemIdArray war hier! useritems.length: " + useritems.length + "<br>");
 	
-	for(i = 0; i < useritems.length; i++) {
-		$("body").append("I[" + i + "]: " + useritems[i] + "<br></br>");
+	if (size > 1024)
+	{
+		size /= 1024;
+		unit = 'KB';
+	} 
+	else if (size > 1024)
+	{
+		size /= 1024;
+		unit = 'MB';
+	} 
+	else if (size > 1024)
+	{
+		size /= 1024;
+		unit = 'GB';
 	}
+	return size.toFixed(1) + ' ' + unit;
 }
-
-
 
 function sortItemsByAttribute(sortAttribute, sortAlgorithmCallback, ascending)
 {
@@ -123,7 +127,9 @@ function resetTable() {
 	$( "#file_table tr").remove();
 
 	$.each( useritems, function( key, item ) {
-			$( "#file_table").append( "<tr class='entry-" + item.id + "'><td><img src='" + item.file_url + "'>" + item.filename + "</td><td id='size-of-" + item.id + "' class='sizeColumn'>" + item.size + "</td><td id='type-of-" + item.id + "' class='typeColumn'>" + item.type + "</td><td id='date-of-" + item.id + "' class='creationColumn'>" + item.creation_date + "</td><td class='textline'><a href='#'><i class='fa fa-eye fa-lg'></i></a><a href='#'><i class='fa fa-share fa-lg'></i></a><p>|</p><a href='#'><i class='fa fa-pencil fa-lg'></i></a><a href='#'><i class='fa fa-lock fa-lg'></i></a><a href='#'><i class='fa  fa-trash-o fa-lg'></i></a></td></tr>" );
+		var size = convertDataSize(item.size);
+	
+		$("#file_table").append( "<tr class='entry-" + item.id + "'><td><a href='" + item.file_url + "' target='_blank'><img id='img-of-" + item.id + "' src='http://wme.lehre.imld.de:8080/wme_13-14/api/items/" + item.id + "/thumbnail'></a>" + item.filename + "</td><td id='size-of-" + item.id + "' class='sizeColumn'>" + size + "</td><td id='type-of-" + item.id + "' class='typeColumn'>" + item.type + "</td><td id='date-of-" + item.id + "' class='creationColumn'>" + item.creation_date + "</td><td class='textline'><a href='#'><i class='fa fa-eye fa-lg'></i></a><a href='#'><i class='fa fa-share fa-lg'></i></a><p>|</p><a href='#'><i class='fa fa-pencil fa-lg'></i></a><a href='#'><i class='fa fa-lock fa-lg'></i></a><a href='#'><i class='fa  fa-trash-o fa-lg'></i></a></td></tr>" );
 	});
 }
 
